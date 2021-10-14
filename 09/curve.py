914566877b0865e5cbb6c350da16418fd5c5ec4d
eAg302 <65862680+eAg302@users.noreply.github.com>
2021-10-14 18:10:54 +0900

    Drill 09 완성
    게임공학과 2019182034
    https://www.youtube.com/watch?v=z2mVBeJ9SGQ

--- 09/curve.py
+++ 09/curve.py
@@ -76,7 +76,7 @@ def draw_curve_4_points(p1, p2, p3, p4):
             draw_point((x, y))
         draw_point(p3)
 
-        # draw p3-p4 최초실행
+        # draw p3-p4
         for i in range(50, 100, 1):
             t = i / 100
             x = (2 * t ** 2 - 3 * t + 1) * p2[0] + (-4 * t ** 2 + 4 * t) * p3[0] + (2 * t ** 2 - t) * p4[0]
